a0e94e876eace2e7494fbe0eff26c4103c61e520
you-oops-dev[silent] <you-oops-dev[silent]@users.noreply.github.com>
2025-02-26 22:55:41 +0300

    Add new domains for GPT client f-droid.org/en/packages/org.woheller69.gptassist/

--- chatgpt/downloader.sh
+++ chatgpt/downloader.sh
@@ -36,7 +36,11 @@ platform.api.openai.com
 platform.openai.com
 spinningup.openai
 chat.openai.com
-chatgpt.com' >> chatgpt/chatgpt_domain.txt
+chatgpt.com
+featureassets.org
+cdnjs.cloudflare.com
+cdn.auth0.com
+prodregistryv2.org' >> chatgpt/chatgpt_domain.txt
 sort chatgpt/chatgpt_domain.txt | uniq | sponge chatgpt/chatgpt_domain.txt
 
 cat chatgpt/chatgpt_domain.txt | utils/mdig-bolvan --family=4 --threads=$(nproc) | sed '/[A-Za-z]/d' | sed 's/ //g' | sed '/^0.0.0.0/d' | sed '/^127.0.0.1/d' | sed '/^1.1.1.1/d' | sed '/^8.8.8.8/d' | sed '/^1.0.0.1/d' | sed '/^8.8.4.4/d' | grepcidr -v '0.0.0.0/8' | grepcidr -v '0.0.0.0/32' | grepcidr -v '10.0.0.0/8' | grepcidr -v '100.64.0.0/10' | grepcidr -v '127.0.0.0/8' | grepcidr -v '169.254.0.0/16' | grepcidr -v '172.16.0.0/12' | grepcidr -v '192.0.0.0/24' | grepcidr -v '192.0.0.0/29' | grepcidr -v '192.0.0.170/32' | grepcidr -v '192.0.0.171/32' | grepcidr -v '192.0.2.0/24' | grepcidr -v '192.88.99.0/24' | grepcidr -v '192.88.99.1/32' | grepcidr -v '192.168.0.0/16' | grepcidr -v '198.51.100.0/24' | grepcidr -v '198.18.0.0/15' | grepcidr -v '203.0.113.0/24' | grepcidr -v '224.0.0.0/4' | grepcidr -v '240.0.0.0/4' | grepcidr -v '255.255.255.255/32' >> /tmp/chatgpt.txt
