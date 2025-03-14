766716186a5f49c384b37f53e573ffbf016b5ae0
naruto522ru[silent] <naruto522ru[silent]@users.noreply.github.com>
2025-03-14 14:01:47 +0300

    Test 3

--- youtube/downloader.sh
+++ youtube/downloader.sh
@@ -8,13 +8,13 @@ set -x
 
 # save ipv4
 #grep -v ':' youtube/nets.txt > /tmp/youtube-ipv4.txt
-curl -4s --max-time 90 --retry-delay 3 --retry 5 https://raw.githubusercontent.com/antonme/ipnames/master/resolve-youtube.txt | grep -i [0-9]. > /tmp/youtube-ipv4.txt || echo 'failed'
-curl -4s --max-time 90 --retry-delay 3 --retry 5 https://raw.githubusercontent.com/antonme/ipnames/master/ext-resolve-youtube.txt | grep -i [0-9]. >> /tmp/youtube-ipv4.txt || echo 'failed'
-
+curl -4s --max-time 90 --retry-delay 3 --retry 5 https://raw.githubusercontent.com/antonme/ipnames/refs/heads/master/resolve-youtube.txt | grep -i [0-9]. > /tmp/youtube-ipv4.txt || echo 'failed'
+curl -4s --max-time 90 --retry-delay 3 --retry 5 https://raw.githubusercontent.com/antonme/ipnames/refs/heads/master/ext-resolve-youtube.txt | grep -i [0-9]. >> /tmp/youtube-ipv4.txt || echo 'failed'
+curl -4s --max-time 90 --retry-delay 3 --retry 5 https://raw.githubusercontent.com/antonme/ipnames/refs/heads/master/dns-youtube.txt >> youtube/domain.txt || echo "YouTube: Getting domain failed"
 # Both Record (IPv4/IPv6)
 #cat youtube/domain.txt | utils/mdig-bolvan --threads=$(nproc) >> /tmp/youtube-ipv4.txt
 # A-Record (IPv4)
-cat youtube/domain.txt | utils/mdig-bolvan --family=4 --threads=$(nproc) >> /tmp/youtube-ipv4.txt || echo "YouTube IPv4: failed"
+cat youtube/domain.txt | sort | uniq | utils/mdig-bolvan --family=4 --threads=$(nproc) >> /tmp/youtube-ipv4.txt || echo "YouTube IPv4: failed"
 # AAAA-Record (IPv6)
 #cat youtube/domain.txt | utils/mdig-bolvan --family=6 --threads=$(nproc) >> /tmp/youtube-ipv6.txt || echo "YouTube IPv6: failed"
 
