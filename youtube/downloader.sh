6605e54f6c238e332798182b926f0af9a10b4e45
naruto522ru[silent] <naruto522ru[silent]@users.noreply.github.com>
2025-03-14 11:04:47 +0300

    Testing

--- youtube/downloader.sh
+++ youtube/downloader.sh
@@ -11,6 +11,8 @@ set -x
 curl -4s --max-time 90 --retry-delay 3 --retry 5 https://raw.githubusercontent.com/antonme/ipnames/master/resolve-youtube.txt | grep -i [0-9]. > /tmp/youtube-ipv4.txt || echo 'failed'
 curl -4s --max-time 90 --retry-delay 3 --retry 5 https://raw.githubusercontent.com/antonme/ipnames/master/ext-resolve-youtube.txt | grep -i [0-9]. >> /tmp/youtube-ipv4.txt || echo 'failed'
 
+dig -f youtube/domain.txt A +short >> youtube/ipv4.txt || echo "YouTube:failed"
+
 # save ipv6
 # grep ':' youtube/nets.txt > /tmp/youtube-ipv6.txt
 
