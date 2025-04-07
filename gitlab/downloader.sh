9b004e003289f095ce233c71f1f1fc98705de282
you-oops-dev[silent] <you-oops-dev[silent]@users.noreply.github.com>
2025-04-07 05:16:47 +0300

    Add sort and uniq in script fix

--- gitlab/downloader.sh
+++ gitlab/downloader.sh
@@ -8,6 +8,8 @@ curl --max-time 30 --retry-delay 3 --retry 10 -4s -# https://raw.githubuserconte
 
 dos2unix gitlab/domains.txt &>/dev/null;
 
+sort gitlab/domains.txt | uniq | sponge gitlab/domains.txt
+
 cat gitlab/domains.txt | utils/mdig-bolvan --family=4 --threads=$(nproc) | sed '/[A-Za-z]/d' | sed 's/ //g' | sed '/^0.0.0.0/d' | sed '/^127.0.0.1/d' | grepcidr -v '0.0.0.0/8' | grepcidr -v '0.0.0.0/32' | grepcidr -v '10.0.0.0/8' | grepcidr -v '100.64.0.0/10' | grepcidr -v '127.0.0.0/8' | grepcidr -v '169.254.0.0/16' | grepcidr -v '172.16.0.0/12' | grepcidr -v '192.0.0.0/24' | grepcidr -v '192.0.0.0/29' | grepcidr -v '192.0.0.170/32' | grepcidr -v '192.0.0.171/32' | grepcidr -v '192.0.2.0/24' | grepcidr -v '192.88.99.0/24' | grepcidr -v '192.88.99.1/32' | grepcidr -v '192.168.0.0/16' | grepcidr -v '198.51.100.0/24' | grepcidr -v '198.18.0.0/15' | grepcidr -v '203.0.113.0/24' | grepcidr -v '224.0.0.0/4' | grepcidr -v '240.0.0.0/4' | grepcidr -v '255.255.255.255/32' > /tmp/gitlab.txt
 cat gitlab/domains.txt | utils/mdig-bolvan --family=6 --threads=$(nproc) | grep ":" >> /tmp/gitlab.txt
 
