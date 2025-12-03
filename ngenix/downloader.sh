9dfe29b784e6963dead34c344688490fdc3235d9
you-oops-dev[silent] <you-oops-dev[silent]@users.noreply.github.com>
2025-12-03 18:07:21 +0300

    Add ngenix

---
+++ ngenix/downloader.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -euo pipefail
+set -x
+
+
+# get from mnt
+get_maintained() {
+    whois -h whois.ripe.net -- "-i mnt-by $1" | rg '^route' | awk '{ print $2; }'
+    whois -h whois.radb.net -- "-i mnt-by $1" | rg '^route' | awk '{ print $2; }'
+    whois -h rr.ntt.net -- "-i mnt-by $1" | rg '^route' | awk '{ print $2; }'
+    whois -h whois.rogerstelecom.net -- "-i mnt-by $1" | rg '^route' | awk '{ print $2; }'
+    whois -h whois.bgp.net.br -- "-i mnt-by $1" | rg '^route' | awk '{ print $2; }'
+}
+
+# get from Autonomous System
+get_routes() {
+    whois -h riswhois.ripe.net -- "-i origin $1" | rg '^route' | awk '{ print $2; }'
+    whois -h whois.radb.net -- "-i origin $1" | rg '^route' | awk '{ print $2; }'
+    whois -h rr.ntt.net -- "-i origin $1" | rg '^route' | awk '{ print $2; }'
+    whois -h whois.rogerstelecom.net -- "-i origin $1" | rg '^route' | awk '{ print $2; }'
+    whois -h whois.bgp.net.br -- "-i origin $1" | rg '^route' | awk '{ print $2; }'
+}
+
+get_maintained 'CCT-MNT' > /tmp/ngenix.txt || echo 'failed'
+get_routes 'AS57363' >> /tmp/ngenix.txt || echo 'failed'
+
+# save ipv4
+grep -v ':' /tmp/ngenix.txt | grep -v "0.0.0.0" > /tmp/ngenix-ipv4.txt
+
+# save ipv6
+grep ':' /tmp/ngenix.txt > /tmp/ngenix-ipv6.txt
+
+# sort & uniq
+sort -h /tmp/ngenix-ipv4.txt | uniq > ngenix/ipv4.txt
+sort -h /tmp/ngenix-ipv6.txt | uniq > ngenix/ipv6.txt
